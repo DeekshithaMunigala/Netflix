93497e89e9efc72e5ab31848d99c2afc5eeacb2e
Munigala Deekshitha <145666932+DeekshithaMunigala@users.noreply.github.com>
2024-07-02 11:41:39 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -3,7 +3,7 @@ export const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTkxOTE0OTIzZWFhYjk3YzU1N2RmYTFmNjYyNzczOSIsIm5iZiI6MTcxOTQ4OTM4NS4zMjA1NDYsInN1YiI6IjY2N2QxOWY5MTRlOTc4Y2I2NDA3YzYxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q6py_o5hWr9Q_b9M4ckRH6nPT3agFLCHnFvUL8aHtBY",
+      "Bearer Your_Token_Here",
   },
 };
 
